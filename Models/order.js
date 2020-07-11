b30a221c00ca895dc89098f7de6a51e43fcdbe6b
moonee <uuuu118@naver.com>
2020-07-12 01:22:13 +0900

    mongoose로 모든 라우팅 구현

--- Models/order.js
+++ Models/order.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 const Product = require('../Models/product');
 const User= require('../Models/user');
 
+/*
+Order 모델에서 효율성을 위해서 한 사람당 하나의 Order 스키마만 생성하도록 하였다.
+따라서 product정보는 productsp{items[]} 로 저장되고, items 내부에는 order를 저장한 product의 Id와, quantity를 저장하였다.
+*/
 const OrderSchema = new Schema({
    products: {
        items:[
@@ -31,15 +35,31 @@ const OrderSchema = new Schema({
     }
 });
 
-OrderSchema.methods.addOrder=function(userId){
-    User.findById(userId)
+OrderSchema.methods.addOrder=function(userId){ // order에 상품 추가- 이미 order에 있는 상품은 수량만 늘리기 
+    return User.findById(userId)
     .then(user=>{
-        this.order.products = user.cart.items;
-    })
-    return this.save();
+        const cart_items = user.cart.items;
+        // 추가해준다. 이미 있는 상품은 quantity를 올려주고 아닌 상품은 추가..
+        let updatedOrderItem=[...this.products.items];
+        for(item of cart_items){
+            const itemIndex = this.products.items.findIndex(cp=>{
+                return cp.productId.toString()==item.productId.toString();
+            })
+            let newQuantity = 1;
+            if(itemIndex>=0){ // 이미 order에 있는 경우 
+                newQuantity+=this.products.items[itemIndex].quantity;
+                updatedOrderItem[itemIndex].quantity=newQuantity;
+            }
+            else{ // 없는 경우 
+                updatedOrderItem.push({productId:item.productId, quantity: item.quantity});
+            }
+        }
+        this.products.items = updatedOrderItem;
+        return this.save();
+    }).catch(err=>console.log(err));
 }
 
-OrderSchema.methods.removeOrder=function(product_id){
+OrderSchema.methods.removeOrder=function(product_id){ // order에서 특정 상품 삭제 
     let updatedOrderItem = this.products.items.filter(item=>{
         return item.productId.toString() !== product_id.toString();
     });
